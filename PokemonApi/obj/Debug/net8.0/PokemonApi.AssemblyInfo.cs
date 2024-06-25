f5f9695aa1d283e836a9c32429c54bcb79e139dd
nnahmed <nnahmed@student.unimelb.edu.au>
2024-06-25 13:01:35 -0500

    minor README updated

--- PokemonApi/obj/Debug/net8.0/PokemonApi.AssemblyInfo.cs
+++ PokemonApi/obj/Debug/net8.0/PokemonApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9dbb77a3700685ea1760b05c6caa394d721fd9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa09be776ab1b97d42f53bab87ece9ff38f2667b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
